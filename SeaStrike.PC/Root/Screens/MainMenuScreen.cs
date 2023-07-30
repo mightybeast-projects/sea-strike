daf6f5c68352ca2c49706831a089a53c66ff992d
MightyBeast <zgpoter@gmail.com>
2023-07-30 23:59:40 +0300

    -feat- : Small changes to main screen.

--- SeaStrike.PC/Root/Screens/MainMenuScreen.cs
+++ SeaStrike.PC/Root/Screens/MainMenuScreen.cs
@@ -14,6 +14,10 @@ public class MainMenuScreen : GameScreen
     private SpriteFont font18;
     private string logo = "Sea Strike";
     private string hint = "Press any button to begin";
+    private Vector2 logoPosition;
+    private Vector2 hintPosition;
+    private int alphaValue = 70;
+    private int fadeIncrement = 3;
 
     public MainMenuScreen(SeaStrike game) : base(game) => this.game = game;
 
@@ -21,31 +25,38 @@ public class MainMenuScreen : GameScreen
     {
         base.LoadContent();
 
-        font32 = Content.Load<SpriteFont>("font/font32");
+        font32 = Content.Load<SpriteFont>("font/font48");
         font18 = Content.Load<SpriteFont>("font/font18");
-    }
-
-    public override void Draw(GameTime gameTime)
-    {
-        game.spriteBatch.Begin();
 
         Vector2 logoV = font32.MeasureString(logo);
         Vector2 hintV = font18.MeasureString(hint);
         Rectangle bounds = game.Window.ClientBounds;
-        Vector2 logoPosition = new Vector2(bounds.Width / 2 - logoV.X / 2, 100);
-        Vector2 hintPosition = new Vector2(bounds.Width / 2 - hintV.X / 2, bounds.Height - hintV.Y * 3);
+        logoPosition = new Vector2(bounds.Width / 2 - logoV.X / 2, 100);
+        hintPosition = new Vector2(bounds.Width / 2 - hintV.X / 2, bounds.Height - hintV.Y * 3);
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        game.GraphicsDevice.Clear(Color.Black);
+
+        game.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
 
         game.spriteBatch.DrawString(font32, logo, logoPosition, Color.White);
-        game.spriteBatch.DrawString(font18, hint, hintPosition, Color.White);
+        game.spriteBatch.DrawString(
+            font18, hint, hintPosition, new Color(255, 255, 255, alphaValue)
+        );
 
         game.spriteBatch.End();
     }
 
     public override void Update(GameTime gameTime)
     {
-        KeyboardState state = Keyboard.GetState();
-
-        if (state.GetPressedKeyCount() > 0)
+        if (Keyboard.GetState().GetPressedKeyCount() > 0)
             game.screenManager.LoadScreen(new SeaStrikeGameScreen(game));
+
+        alphaValue += fadeIncrement;
+
+        if (alphaValue >= 255 || alphaValue <= 70)
+            fadeIncrement *= -1;
     }
 }
