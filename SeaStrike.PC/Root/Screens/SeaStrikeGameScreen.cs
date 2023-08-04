e5ee5a4782aa6631b330144a9a7b9cd40c50e75b
MightyBeast <zgpoter@gmail.com>
2023-08-04 18:01:48 +0300

    -feat- : Added board grid.

--- SeaStrike.PC/Root/Screens/SeaStrikeGameScreen.cs
+++ SeaStrike.PC/Root/Screens/SeaStrikeGameScreen.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using MonoGame.Extended.Screens;
+using Myra.Graphics2D;
 using Myra.Graphics2D.Brushes;
 using Myra.Graphics2D.UI;
 
@@ -8,6 +9,7 @@ namespace SeaStrike.PC.Root.Screens;
 public class SeaStrikeGameScreen : GameScreen
 {
     private readonly SeaStrike game;
+    private Grid mainGrid;
 
     public SeaStrikeGameScreen(SeaStrike game) : base(game) => this.game = game;
 
@@ -15,7 +17,7 @@ public class SeaStrikeGameScreen : GameScreen
     {
         base.LoadContent();
 
-        Grid mainGrid = new Grid()
+        mainGrid = new Grid()
         {
             ShowGridLines = true,
             GridLinesColor = Color.Magenta
@@ -24,37 +26,9 @@ public class SeaStrikeGameScreen : GameScreen
         mainGrid.RowsProportions.Add(new Proportion(ProportionType.Auto));
         mainGrid.ColumnsProportions.Add(new Proportion(ProportionType.Pixels, 300));
 
-        Label phaseLabel = new Label()
-        {
-            Text = "Deployment phase",
-            TextColor = Color.LawnGreen,
-            Font = game.fontSystem.GetFont(40),
-            GridColumnSpan = 2,
-            HorizontalAlignment = HorizontalAlignment.Center
-        };
-
-        Panel shipsPanel = new Panel()
-        {
-            Width = 200,
-            Height = 300,
-            GridRow = 1,
-            Background = new SolidBrush(Color.Red),
-            VerticalAlignment = VerticalAlignment.Center
-        };
-        Panel boardPanel = new Panel()
-        {
-            Width = 350,
-            Height = 350,
-            GridRow = 1,
-            GridColumn = 1,
-            Background = new SolidBrush(Color.Red),
-            HorizontalAlignment = HorizontalAlignment.Center,
-            VerticalAlignment = VerticalAlignment.Center
-        };
-
-        mainGrid.Widgets.Add(phaseLabel);
-        mainGrid.Widgets.Add(shipsPanel);
-        mainGrid.Widgets.Add(boardPanel);
+        AddPhaseLabel();
+        AddShipsPanel();
+        AddBoardPanel();
 
         game.desktop.Root = mainGrid;
     }
@@ -66,7 +40,91 @@ public class SeaStrikeGameScreen : GameScreen
         game.desktop.Render();
     }
 
-    public override void Update(GameTime gameTime)
+    public override void Update(GameTime gameTime) { }
+
+    private void AddPhaseLabel()
     {
+        Label phaseLabel = new Label()
+        {
+            Text = "Deployment phase",
+            TextColor = Color.LawnGreen,
+            Font = game.fontSystem.GetFont(40),
+            GridColumnSpan = 2,
+            HorizontalAlignment = HorizontalAlignment.Center
+        };
+
+        mainGrid.Widgets.Add(phaseLabel);
+    }
+
+    private void AddShipsPanel()
+    {
+        Panel shipsPanel = new Panel()
+        {
+            Width = 200,
+            Height = 300,
+            GridRow = 1,
+            Border = new SolidBrush(Color.LawnGreen),
+            BorderThickness = new Thickness(1),
+            VerticalAlignment = VerticalAlignment.Center
+        };
+
+        mainGrid.Widgets.Add(shipsPanel);
+    }
+
+    private void AddBoardPanel()
+    {
+        Panel boardPanel = new Panel()
+        {
+            Width = 345,
+            Height = 345,
+            GridRow = 1,
+            GridColumn = 1,
+            Border = new SolidBrush(Color.LawnGreen),
+            BorderThickness = new Thickness(2),
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center
+        };
+
+        Grid boardGrid = new Grid()
+        {
+            ShowGridLines = true,
+            GridLinesColor = Color.Green
+        };
+
+        for (int i = 0; i < 12; i++)
+        {
+            boardGrid.RowsProportions.Add(new Proportion(ProportionType.Pixels, (float)boardPanel.Width / 11));
+            boardGrid.ColumnsProportions.Add(new Proportion(ProportionType.Pixels, (float)boardPanel.Height / 11));
+        }
+
+        for (int i = 1; i < 11; i++)
+        {
+            Label label = new Label()
+            {
+                Text = i.ToString(),
+                Font = game.fontSystem.GetFont(24),
+                GridColumn = i,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            boardGrid.Widgets.Add(label);
+        }
+
+        for (int i = 1; i < 11; i++)
+        {
+            Label label = new Label()
+            {
+                Text = ((char)(i + 64)).ToString(),
+                Font = game.fontSystem.GetFont(24),
+                GridRow = i,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            boardGrid.Widgets.Add(label);
+        }
+
+        boardPanel.AddChild(boardGrid);
+
+        mainGrid.Widgets.Add(boardPanel);
     }
 }
