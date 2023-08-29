ab442d2137f42c68e5f3337948b8646e0d5232c8
MightyBeast <zgpoter@gmail.com>
2023-08-29 13:03:04 +0300

    -feat- : Added multiplayer button on main menu screen.

--- SeaStrike.PC/Root/Screens/MainMenuScreen.cs
+++ SeaStrike.PC/Root/Screens/MainMenuScreen.cs
@@ -2,12 +2,14 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended.Screens;
 using Myra.Graphics2D.UI;
+using SeaStrike.PC.Root.Widgets;
 
 namespace SeaStrike.PC.Root.Screens;
 
 public class MainMenuScreen : GameScreen
 {
     private readonly SeaStrike game;
+    private Panel mainPanel;
 
     public MainMenuScreen(SeaStrike game) : base(game) => this.game = game;
 
@@ -15,8 +17,16 @@ public class MainMenuScreen : GameScreen
     {
         base.LoadContent();
 
-        Panel mainPanel = new Panel();
+        mainPanel = new Panel();
 
+        AddTitleLabel();
+        AddGameModesPanel();
+
+        game.desktop.Root = mainPanel;
+    }
+
+    private void AddTitleLabel()
+    {
         mainPanel.Widgets.Add(new Label()
         {
             Text = "SEA STRIKE",
@@ -26,25 +36,45 @@ public class MainMenuScreen : GameScreen
             VerticalAlignment = VerticalAlignment.Top,
             Top = 100,
         });
+    }
 
-        mainPanel.Widgets.Add(new Label()
+    private void AddGameModesPanel()
+    {
+        VerticalStackPanel panel = new VerticalStackPanel()
         {
-            Text = "Press any key to start...",
+            Spacing = 20,
+            Top = -100,
+            VerticalAlignment = VerticalAlignment.Bottom
+        };
+
+        panel.Widgets.Add(new Label()
+        {
+            Text = "Choose game mode :",
+            Font = SeaStrike.fontSystem.GetFont(30),
             TextColor = Color.LawnGreen,
-            Font = SeaStrike.fontSystem.GetFont(32),
-            HorizontalAlignment = HorizontalAlignment.Center,
-            VerticalAlignment = VerticalAlignment.Bottom,
-            Top = -100
+            HorizontalAlignment = HorizontalAlignment.Center
         });
 
-        game.desktop.Root = mainPanel;
+        GameButton singlePlayerButton = new GameButton()
+        {
+            Text = "Single player"
+        };
+        singlePlayerButton.TouchUp += (s, a) =>
+            game.screenManager.LoadScreen(new DeploymentPhaseScreen(game));
+
+        panel.Widgets.Add(singlePlayerButton);
+
+        GameButton multiplayerButton = new GameButton()
+        {
+            Text = "Local multiplayer"
+        };
+
+        panel.Widgets.Add(multiplayerButton);
+
+        mainPanel.Widgets.Add(panel);
     }
 
     public override void Draw(GameTime gameTime) { }
 
-    public override void Update(GameTime gameTime)
-    {
-        if (Keyboard.GetState().GetPressedKeyCount() > 0)
-            game.screenManager.LoadScreen(new DeploymentPhaseScreen(game));
-    }
+    public override void Update(GameTime gameTime) { }
 }
